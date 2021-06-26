50bab99462f4b8db3a9feab9530de8bce96b1dd7
caoligang <1296933@qq.com>
2021-06-26 23:47:45 +0800

    add remote data gdal

--- pyqtmain.py
+++ pyqtmain.py
@@ -1,12 +1,27 @@
 import sys
 import mainWindow
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import os
+from osgeo import gdal
+from osgeo import ogr
+import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ =="__main__":
-    app =QApplication(sys.argv)
-    mainwin=QMainWindow()
-    ui=mainWindow.Ui_MainWindow()
-    ui.setupUi(mainwin)
-    mainwin.show()
 
-    sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWin = QMainWindow()
+    ui = mainWindow.Ui_MainWindow()
+    ui.setupUi(mainWin)
+    mainWin.show()
+
+
+
+
+filePath=r'/Users/caoligang/remote/110000BJ_L5_TM_1990.tif'
+ds=gdal.Open(filePath)
+dir(ds)
+print(ds.GetMetadata())
+print(ds.RasterCount)
+
+  #  sys.exit(app.exec_())
